9cd40cbd0aa08360264851ab3b4f1f2412e601d8
jkobject <jkobject@gmail.com>
2024-09-05 17:11:00 +0200

    better tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -5,6 +5,7 @@ from scipy.sparse import csr_matrix
 
 from grnndata import GRNAnnData
 from bengrn.base import NAME, BenGRN
+import pytest
 
 
 def test_base():
@@ -14,7 +15,9 @@ def test_base():
     adata = adata[:, np.argsort(-adata.X.sum(axis=0)).tolist()[0][:1000]]
     random_mask = np.random.choice([0, 1], size=random_matrix.shape, p=[0.8, 0.2])
     sparse_random_matrix = csr_matrix(random_matrix * random_mask)
-    grn = GRNAnnData(adata.copy(), grn=sparse_random_matrix)
-    grn.var.index = grn.var.symbol.astype(str)
-    metrics = BenGRN(grn, doplot=False).scprint_benchmark()
-    assert True, "BenGRN test passed"
+    try:
+        grn = GRNAnnData(adata.copy(), grn=sparse_random_matrix)
+        grn.var.index = grn.var.symbol.astype(str)
+        _ = BenGRN(grn, doplot=False).scprint_benchmark()
+    except Exception as e:
+        pytest.fail(f"An exception occurred: {str(e)}")
